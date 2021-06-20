a6f29f7037bf8cb9d3d0e074ceb8d43289635758
MykolaMatsko <mykola.matsko@gmail.com>
2021-06-20 15:16:47 +0300

    41 styling the Sidenav added.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,15 @@ import {NgModule} from "@angular/core";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatInputModule],
-  exports: [MatButtonModule, MatIconModule, MatInputModule]
+  imports: [MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+  exports: [MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
 })
-export class MaterialModule{}
+export class MaterialModule {
+}
